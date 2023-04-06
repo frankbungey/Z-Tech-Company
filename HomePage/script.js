3860068d87c5754ea1818f32dfdee7287135e5c7
frankbungey <frankbungey@gmail.com>
2023-04-06 18:56:27 +0300

    ZTech company website

---
+++ HomePage/script.js
@@ -0,0 +1,6 @@
+const explore = document.querySelector(".explore").addEventListener("click",function(){
+       location.href="/ProductsPage/index1.html"
+})
+const linkOne = document.querySelector(".link1").addEventListener("click",function(){
+    location.href="/ProductsPage/index1.html"
+})
