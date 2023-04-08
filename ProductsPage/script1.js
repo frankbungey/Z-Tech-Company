aa347d6f7123e06627dde6a0997bf28db9eb2ef4
frankbungey <frankbungey@gmail.com>
2023-04-08 20:22:44 +0300

    You can Remove Items From the Cart

--- ProductsPage/script1.js
+++ ProductsPage/script1.js
@@ -5,35 +5,35 @@ const items = [
         id: "1",
         name: "Laptop",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit augue ut tortor faucibus convallis.",
-        amount: "400",
+        amount: "$400",
         imageUrl: "/Z-Tech-Company/IMAGES/pic5.jpg"
     },
     {
         id: "2",
         name: "Fridge",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit augue ut tortor faucibus convallis.",
-        amount: "300",
+        amount: "$300",
         imageUrl: "/Z-Tech-Company/IMAGES/pic5.jpg"
     },
     {
         id: "3",
         name: "Smart tv",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit augue ut tortor faucibus convallis.",
-        amount: "200",
+        amount: "$200",
         imageUrl: "/Z-Tech-Company/IMAGES/comp1.jpg"
     },
     {
         id: "4",
         name: "Heater",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit augue ut tortor faucibus convallis.",
-        amount: "15",
+        amount: "$15",
         imageUrl: "/Z-Tech-Company/IMAGES/pic5.jpg"
     },
     {
         id: "5",
         name: "Woofer  ",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit augue ut tortor faucibus convallis.",
-        amount: "50",
+        amount: "$50",
         imageUrl: "/Z-Tech-Company/IMAGES/comp1.jpg"
     }
 ]
@@ -44,8 +44,9 @@ items.forEach(item => {
     <div class="products" >
     <img class="img" src="${item.imageUrl}" height="200px" width="300px">
       <h2 class="des"><u>${item.name}</u></h2>
-    <p>
-          ${item.description}
+    <p>   
+      Amount:${item.amount} <br><br>
+        ${item.description}
     </p>
     <input class="btn1" type="button" value="Add to Cart" id="${item.id}">
 </div>
